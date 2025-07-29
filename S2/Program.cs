6488c2e2baed92770aa689a6b0762a9bc11d2ef7
Amidooovich <144342730+Amidoovich@users.noreply.github.com>
2025-07-30 00:43:22 +0300

    Part 03

--- S2/Program.cs
+++ S2/Program.cs
@@ -4,27 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Point P01; // declare variable from point data type
-            // CLR will Allocate 8 Unintialized Bytes in Stack
-            //== int X;
+            #region Struct
+            #region Example 01
+            //Point P01; // declare variable from point data type
+            //// CLR will Allocate 8 Unintialized Bytes in Stack
+            ////== int X;
 
-            //P01 = new Point();
+            ////P01 = new Point();
+            ////// new => just for constructor selection that will used to initialize P01 Attributes
+
+            ////Console.WriteLine(P01.X);
+            ////Console.WriteLine(P01.Y);
+
+            //P01 = new Point(10);
             //// new => just for constructor selection that will used to initialize P01 Attributes
 
             //Console.WriteLine(P01.X);
             //Console.WriteLine(P01.Y);
 
-            P01 = new Point(10);
-            // new => just for constructor selection that will used to initialize P01 Attributes
+            //Console.WriteLine(P01);
+            //Console.WriteLine(P01.ToString());
 
-            Console.WriteLine(P01.X);
-            Console.WriteLine(P01.Y);
+            //int X = 5;
+            //Console.WriteLine(X);  
+            #endregion
+            #region Example 02
+            //Point P01 = new Point(10,20);
 
-            Console.WriteLine(P01);
-            Console.WriteLine(P01.ToString());
+            //Console.WriteLine(P01);
 
-            int X = 5;
-            Console.WriteLine(X);
+
+
+            //Point P02 = new Point(100,200);
+            //Console.WriteLine(P02);
+
+            //P02 = P01;
+
+            //Console.WriteLine(P01);
+            //Console.WriteLine(P02);
+
+            //P02.X = 300;
+            
+            //Console.WriteLine(P01);
+            //Console.WriteLine(P02);
+
+
+            #endregion
+            #endregion
 
 
         }
