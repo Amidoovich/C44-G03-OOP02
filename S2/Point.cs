078617f6d656ff0c416f70f9613fe7b4cd0ef46a
Amidooovich <144342730+Amidoovich@users.noreply.github.com>
2025-07-30 00:36:00 +0300

    Part 02

---
+++ S2/Point.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S2
+{
+    internal struct Point
+    {
+        #region Attributes
+        public int X;
+        public int Y;
+        #endregion
+
+        #region Constructors
+        // is a special function
+        //      1. Named As Struct
+        //      2. Has No Return Type
+        //      3. Intialize Attributes
+
+        // Paramter Less Constructor
+        // Access After .Net 5.0
+        public Point()
+        {
+            X = default;
+            Y = default;
+        }
+
+        public Point(int _x ,int _y)
+        {
+            //this.X = X;
+            //this.Y = Y;
+            X= _x;
+            Y= _y;
+        }
+
+        public Point(int Number)
+        {
+            X = Y = Number;
+        }
+
+
+
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return $"({X} , {Y})";
+        }
+
+        #endregion
+    }
+}
--- S2/Program.cs
+++ S2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Point P01; // declare variable from point data type
+            // CLR will Allocate 8 Unintialized Bytes in Stack
+            //== int X;
+
+            //P01 = new Point();
+            //// new => just for constructor selection that will used to initialize P01 Attributes
+
+            //Console.WriteLine(P01.X);
+            //Console.WriteLine(P01.Y);
+
+            P01 = new Point(10);
+            // new => just for constructor selection that will used to initialize P01 Attributes
+
+            Console.WriteLine(P01.X);
+            Console.WriteLine(P01.Y);
+
+            Console.WriteLine(P01);
+            Console.WriteLine(P01.ToString());
+
+            int X = 5;
+            Console.WriteLine(X);
+
+
         }
     }
 }
