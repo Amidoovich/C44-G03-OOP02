3c2c848e96769f33acc0077ee969e56adbaa3cc9
Amidooovich <144342730+Amidoovich@users.noreply.github.com>
2025-07-30 18:13:05 +0300

    Q2

--- S2/Point.cs
+++ S2/Point.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace S2
 {
-    internal class Point
+    internal struct Point
     {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+
+        #region constructors
+
+        public Point(int x , int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public static float DistanceBetweenTwoPoints(Point P01 , Point P02)
+        {
+            return (float) Math.Sqrt(((P01.X - P02.X) * (P01.X - P02.X)) + ((P01.Y - P02.Y) * (P01.Y - P02.Y)));
+        }
+
+        #endregion
     }
 }
--- S2/Program.cs
+++ S2/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace S2
+﻿using System.Xml;
+
+namespace S2
 {
     internal class Program
     {
@@ -6,18 +8,59 @@
         {
             #region Q1-Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
 
-            Person[] Persons = new Person[3];
+            //Person[] Persons = new Person[3];
 
-            Persons[0] = new Person("Ahmed",21);
-            Persons[1] = new Person("Mohamed",50);
-            Persons[2] = new Person("Samy",79);
-            
+            //Persons[0] = new Person("Ahmed",21);
+            //Persons[1] = new Person("Mohamed",50);
+            //Persons[2] = new Person("Samy",79);
 
-            for (int i = 0;i<Persons.Length;i++)
-                Console.WriteLine($"Person Number {i+1}\nName : {Persons[i].Name}\nAge : {Persons[i].Age}");
+
+            //for (int i = 0;i<Persons.Length;i++)
+            //    Console.WriteLine($"Person Number {i+1}\nName : {Persons[i].Name}\nAge : {Persons[i].Age}");
 
 
             #endregion
+
+            #region Q2-Create a struct called "Point" to represent a 2D point with properties "X" and   "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.
+            //bool IsParse;
+            //Point P01 = new Point();
+            //Point P02 = new Point();
+            //int X, Y;
+            //do
+            //{
+            //    Console.Write("please enter the X-axis for point 1  : ");
+            //    IsParse = int.TryParse(Console.ReadLine(), out X);
+
+            //} while (!IsParse);
+            //do
+            //{
+            //    Console.Write("please enter the Y-axis for point 1  : ");
+            //    IsParse = int.TryParse(Console.ReadLine(), out Y);
+
+            //} while (!IsParse);
+
+            //P01.X = X;
+            //P01.Y = Y;
+
+            //do
+            //{
+            //    Console.Write("please enter the X-axis for point 2  : ");
+            //    IsParse = int.TryParse(Console.ReadLine(), out X);
+
+            //} while (!IsParse);
+            //do
+            //{
+            //    Console.Write("please enter the Y-axis for point 2  : ");
+            //    IsParse = int.TryParse(Console.ReadLine(), out Y);
+
+            //} while (!IsParse);
+
+            //P02.X = X;
+            //P02.Y = Y;
+
+            //Console.WriteLine($"the distance is {Point.DistanceBetweenTwoPoints(P01,P02)}");
+            #endregion
+
         }
 
     }
